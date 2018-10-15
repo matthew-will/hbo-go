06d628412698e8fa53e0cce227a7577981df3938
matthew-will <matt.will@weareglow.com>
2018-10-15 11:39:25 -0400

    Project setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo"
+         src="./assets/logo.png">
+    <Landing msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from './components/Landing.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Landing
   }
 }
 </script>
